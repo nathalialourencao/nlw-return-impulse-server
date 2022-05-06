98df4a42305a84f695663af64eedf2742400e193
Nathália Lourenção <nathalia.lourencao@fcamara.com.br>
2022-05-06 20:03:36 -0300

    Ajustes para deploy

--- dist/server.js
+++ dist/server.js
@@ -1,2 +1,16 @@
 "use strict";
-const teste = 1;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const routes_1 = require("./routes");
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json({ limit: '50mb' }));
+// app.use(express.json());
+app.use(routes_1.routes);
+app.listen(3333, () => {
+    console.log('HTTP server running!');
+});
